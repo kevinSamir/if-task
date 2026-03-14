9cc1880aa12ab889e43637482c886c9dc1da2b41
Nour <Kevinsamir714@gmail.com>
2026-03-14 02:25:06 +0200

    first commit

--- lip/main.dart
+++ lip/main.dart
@@ -1,30 +1,39 @@
-import "dart:io";
-void main(){
-    print ('enter your name');
-    String name =(stdin. readLineSync()!);
-    print ('enter your age ');
-    double age = double.parse (stdin .readLineSync()!);
-    print ('enter your grade  ');
-    double grade = double.parse (stdin .readLineSync()!);
-    print ('your name is $name\n your age is $age   ');
-    if (grade <50 ){
-        print('and  you is faill ');
-    }
-   else  if(grade >50 && grade <60) {
-        print("and  you is acceptable ");
-    }
-    else if(grade >60 && grade <70){
-    print("and you is good ");
-    }
-    else if (grade >70 && grade <80 ){
-    print("and  you is very good  ");
+import 'dart:io';
 
-    }
-    else if (grade >80 && grade <=100 ) {
-        print ("and  you is  privilege");
-    }
+void main() {
 
+  print("hello in our calculator");
 
+  print("enter the first number");
+  int number1 = int.parse(stdin.readLineSync()!);
 
-    
-    }
+  print("enter the second number");
+  int number2 = int.parse(stdin.readLineSync()!);
+
+  num result = 0;
+
+  print("you have number 1 is $number1 : number 2 is $number2");
+  print("what do you want?");
+  print("1- mult");
+  print("2- divide");
+  print("3- minus");
+  print("4- plus");
+
+  print("enter your selection");
+  int selection = int.parse(stdin.readLineSync()!);
+
+  if (selection == 1) {
+    result = number1 * number2;
+  } 
+  else if (selection == 2) {
+    result = number1 / number2;
+  } 
+  else if (selection == 3) {
+    result = number1 - number2;
+  } 
+  else {
+    result = number1 + number2;
+  }
+
+  print("your result is $result");
+}
